98bd64a34899c54f2364544ca6974de5de70cf8d
lol_James <119302326+lol-James@users.noreply.github.com>
2023-04-25 21:57:07 +0800

    main

--- 程式作業#13/square.cpp
+++ 程式作業#13/square.cpp
@@ -9,32 +9,23 @@ using namespace std;
 
 double sum_x, sum_y;
 
-inline void sum(double a1, int x, double &sum)
-{
-    sum += a1 * (pow(4, x + 1) - 1) / 3 - a1;
-}
-
 void calcCoords(int x, double a, double b, double d)
 {
-    if (x == 0 || a + d < 0 || b + d < 0)
+    if (x == 0 || d == 0 || a + d < 0 || b + d < 0)
         return;
 
     if (a - d >= 0 && b - d >= 0)
     {
-        sum(a, x, sum_x);
-        sum(b, x, sum_y);
+        sum_x += a * (pow(4, x + 1) - 1) / 3 - a;
+        sum_y += b * (pow(4, x + 1) - 1) / 3 - b;
         return;
     }
 
     double dist = d / 2.0;
-
     double x1 = a - dist, y1 = b - dist;
-    double x2 = a + dist, y2 = b - dist;
-    double x3 = a - dist, y3 = b + dist;
-    double x4 = a + dist, y4 = b + dist;
-
-    if (x4 + dist < 0 || y4 + dist < 0)
-        return;
+    double x2 = a + dist, y2 = y1;
+    double x3 = x1, y3 = b + dist;
+    double x4 = x2, y4 = y3;
 
     if (x1 > 0 && y1 > 0)
     {
