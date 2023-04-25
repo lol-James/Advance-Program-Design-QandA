5191f417b6653e089165928f145161962ced306b
lol_James <119302326+lol-James@users.noreply.github.com>
2023-04-25 17:30:14 +0800

    main

---
+++ 程式作業#13/square.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double x_sum = 0, y_sum = 0;
+
+inline void find_vertices(double a, double b, double d, double square[][2]);
+inline void draw(double a, double b, double d, double x);
+inline double G_sum(double a1, double n);
+
+int main()
+{
+    double a, b, d, x;
+    while (cin >> a >> b >> d >> x)
+    {
+        x_sum = y_sum = 0;
+
+        if (a > 0 && b > 0)
+        {
+            x_sum += a;
+            y_sum += b;
+        }
+
+        draw(a, b, d, x);
+
+        cout << x_sum << " " << y_sum << endl;
+    }
+
+    system("pause");
+    return 0;
+}
+
+inline void find_vertices(double a, double b, double d, double square[][2])
+{
+    square[0][0] = a + d / 2;
+    square[0][1] = b + d / 2;
+    square[1][0] = a - d / 2;
+    square[1][1] = b + d / 2;
+    square[2][0] = a - d / 2;
+    square[2][1] = b - d / 2;
+    square[3][0] = a + d / 2;
+    square[3][1] = b - d / 2;
+}
+
+inline void draw(double a, double b, double d, double x)
+{
+    double square[4][2];
+
+    find_vertices(a, b, d, square);
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (square[i][0] > 0 && square[i][1] > 0)
+        {
+            x_sum += square[i][0];
+            y_sum += square[i][1];
+        }
+    }
+
+    if (x)
+    {
+        if (a + d < 0 && b + d < 0)
+            return;
+        else
+            draw(a + d / 2, b + d / 2, d / 2, x - 1);
+
+        if (a < 0 && b + d < 0)
+            return;
+        else
+            draw(a - d / 2, b + d / 2, d / 2, x - 1);
+
+        if (a < 0 && b < 0)
+            return;
+        else
+            draw(a - d / 2, b - d / 2, d / 2, x - 1);
+
+        if (a + d < 0 && b < 0)
+            return;
+        else
+            draw(a + d / 2, b - d / 2, d / 2, x - 1);
+    }
+}
